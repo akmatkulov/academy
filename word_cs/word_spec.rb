7a8d7cbd2c7c5392be6f89708050a028f1bee93e
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-02-06 14:03:22 +0600

    RSpec test fixed

--- word_cs/word_spec.rb
+++ word_cs/word_spec.rb
@@ -3,9 +3,9 @@ require './word.rb'
 
 RSpec.describe "word_cs" do
   it "word_cs numbers" do
-     expect(word_cs("seedcs")).to eq("Include 'cs': 64")
+     expect(word_cs("seedCS")).to eq("Include 'CS': 64")
   end
   it "word_cs reverse" do
-      expect(word_cs("select")).to eq("tceles")
+      expect(word_cs("seedcs")).to eq("scdees")
   end
 end
